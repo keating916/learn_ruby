f56285194ec58aa2f8bf3c3ecdd0dbf87fb0ee0c
Patrick Keating <keating916@gmail.com>
2019-07-07 19:25:36 -0700

    add multiply work on power

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(a,b)
+    a + b
+end
+
+def subtract(a,b)
+    a-b
+end
+
+def sum(array)
+    x = array.reduce(:+)
+    if(x == nil) 
+        x = 0
+    else 
+        x
+    end
+end
+
+def multiply(array)
+    x = array.reduce(:*)
+    if(x == nil) 
+        x = 0
+    else 
+        x
+    end
+end
+
+def power(a,b)
+    b.times do
+        a * a
+    end
+end
