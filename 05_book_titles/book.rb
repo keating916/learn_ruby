98730b66990ef482daa3da27090c85301a9a8dac
Patrick Keating <keating916@gmail.com>
2019-07-12 14:33:20 -0700

    complete solutions

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+    attr_reader :title
+
+    def title=(s)
+        @title = titleize(s)
+        @title 
+    end
+
+    def titleize(string)
+        if(string.split(" ").length > 1)
+            out = string.split.map{ |i| 
+                if(i != "a" && i != "the" && i != "of" && i != "and" && i != "over" && i != "in" && i != "an") 
+                    i.capitalize
+                else 
+                    i 
+                end
+            }
+            if(out[0].capitalize! != nil)
+                out[0].capitalize
+            end
+            out = out.join(" ")
+        else 
+            out = string.capitalize
+        end
+        
+        out
+    end
+
 end
