c6edf8790561d87b4a4a6afbca9c6d9780616c23
Patrick Keating <keating916@gmail.com>
2019-07-12 15:22:55 -0700

    complete timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    m = @seconds/60
+    h = m/60
+    if(h != 0)
+      m = m %60
+    end
+    @seconds = @seconds %60
+    return "%02i:%02i:%02i" % [h, m, @seconds]
+  end
 end
